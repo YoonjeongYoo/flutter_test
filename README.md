b0ee23484fde1389f9e8e3adff9a113c8b705bbf
Yoonj310 <opi2982@gmail.com>
2024-05-10 23:02:52 +0900

    readme test 수정

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ A new Flutter project.
 ## 수정해보기
 
 추가 수정해서 update project test
+
+
+test 추가
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,163 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  //출력
+  /*print('Hello Code Factory');*/
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  //변수 - variable
+  /*var name = '코드팩토리';
+  print(name);
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  var name2 = '레드벨벳';
+  print(name2);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  name = '플러터 프로그래밍'
+  print(name);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  //var name = '코드팩토리2'; => 오류, 이미 name 변수 사용중*/
 
-  final String title;
+  //변수 타입 - 정수(int)
+  /*int number1 = 10;
+  print(number1);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  int number2 = 15;
+  print(number2)
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  print(number1 + number2); // 더하기
+  print(number1 - number2); // 빼기
+  print(number1 / number2); // 나누기
+  print(number1 * number2); // 곱하기*/
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  //변수 타입 - 실수(double)
+  /*double number1 = 2.5;
+  double number2 = 0.5;
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  print(number1 + number2); // 더하기
+  print(number1 - number2); // 빼기
+  print(number1 / number2); // 나누기
+  print(number1 * number2); // 곱하기*/
+
+  // 변수 타입 - Boolean
+  /*bool isTrue = true;
+  bool isFalse = false;
+
+  print(isTrue);
+  print(isFalse);*/
+
+  // 변수 타입 - 글자(String)
+  /*String name = '레드벨벳';
+  String name2 = '슬기';
+
+  print(name);
+  print(name2);
+  print(name + ' ' +name2);
+
+  print('${name.runtimeType} ${name2}'); // => 함수까지 인식 가능
+  print('$name.runtimeType $name2'); // => 단순 변수까지만 인식해서 출력가능*/
+
+  //var VS String => var는 오른쪽 값에 따라 변수타입을 유추해서 타입 지정
+
+  //dynamic
+  /*dynamic name = '코드팩토리';
+  print(name);
+
+  dynamic name = 1;
+  print(number);
+
+  var name2 = '블랙핑크';
+  print(name2);
+
+  //var VS dynamic => var는 한번 선언된 변수 타입으로 지정하지 않으면 오류, 반대로 dynamic은 변수 타입 변경 가능
+  name = 2;
+  name2 = 5; // -> 이 경우에는 오류*/
+
+  //nullable - null이 될 수 있다.
+  //non-nullable - null이 될 수 없다.
+  //null - 아무런 값도 있지 않다.
+  /*String name = '코드팩토리';
+  print(name);
+
+  String? name2 = '블랙핑크';
+  name2 = null; // -> String? name2 는 null을 넣을 수 있다.
+  print(name2);
+  //print(name2!); // -> 현재 name2의 값은 null이 아니다를 !로표현*/
+
+  //final과 const 공통점 => 한번 선언하면 값을 변경할 수 없다, var 생략가능
+  /*final String name = '코드팩토리';
+  print(name);
+  //name = '블랙핑크'; // -> 에러 발생
+
+  const String name2 = '블랙핑크';
+  print(name2);
+  //name = '레드벨벳'; // -> 에러 발생*/
+
+  //DateTime
+  /*DateTime now = DateTime.now();
+  print(now);*/
+
+  //final과 const 차이점
+  /*final DateTime now = DateTime.now();
+  print(now);
+
+  //const DateTime now2 = DateTime.now(); -> 에러 발생 why? => const 타입은 빌드타임의 값을 알고있어야 함, DateTime.now()메소드의 경우 빌드타임의 값을 미리 알 수 없다.*/
+
+  //추가 연산자
+  /*int number = 2;
+  print(number % 2); //나머지
+  print('---------------------');
+  number ++;
+  print(number);
+  print('---------------------');
+  number --;
+  print(number);
+  print('---------------------');
+  number += 2;
+  print(number);
+  print('---------------------');
+  double? number2 = 4.0;
+  print(number2);
+  number2 = null;
+  print(number2);
+  number2 ??= 3.0; // ??= -> 이 연산자는 number2가 null이면 오른쪽 값으로 값을 변경하라는 의미이다.
+  print(number2);*/
+
+  //타입 확인
+  /*int number1 = 1;
+
+  print(number1 is int);
+  print(number1 is String);
+  print(number1 is! int);
+  print(number1 is! String);*/
+
+  //List
+  /*List<String> blackPink = ['제니','지수', '로제', '리사'];
+  List<int> numbers = [1, 2, 3, 4, 5, 6];
+
+  print(blackPink);
+  print(numbers);
+
+  //index
+  print(blackPink[0]);
+  print(blackPink[1]);
+  //print(blackPink[4]); // => 에러 발생
+
+  print(blackPink.length); // 길이 또는 갯수를 셀때
+  blackPink.add('코드팩토리');
+  print(blackPink);
+
+  blackPink.remove('코드팩토리');
+  print(blackPink);
+
+  print(blackPink.indexOf('로제'));*/
+
+  //Map - Key / Value
+  /*Map<String, String> dictionary = {
+    '유윤정' : 'yyj',
+  };
+
+  Map<String, bool> dictionary2 = {
+    '유윤정' : true,
+  };*/
+
 }
